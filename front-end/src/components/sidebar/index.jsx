64153ae1b9499fbbd1ef795258e79f0a3ae5cab3
Vivek Katkar <102805058+vivekkatkar@users.noreply.github.com>
2024-07-15 08:14:39 +0530

    Dashboard Page Added

---
+++ front-end/src/components/sidebar/index.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import Logo from "../../assets/images/logo.png"
+import Button from '@mui/material/Button';
+import { MdOutlineDashboard, MdProductionQuantityLimits, MdKeyboardArrowRight } from "react-icons/md";
+
+
+function MenuItem(title, icon){
+    return (
+        <div className="side-menu py-1 pl-3 flex items-center">
+            <Button className="w-100 items-center"> 
+                <div className="flex items-center">
+                    <span className="icon w-[30px] h-[30px] flex items-center justify-center rounded-md "> {icon}  </span>
+                    {title} 
+                </div>
+                <div className="w-[30px] h-[30px] flex pt-1">
+                    <MdKeyboardArrowRight />
+                </div>
+            </Button>
+           
+        </div>
+    );
+}
+
+export default function SideBar(){
+    return <>
+        <div className="sidebar fixed top-0 left-0 z-[100] h-full ">
+            <div className="border flex justify-center items-center py-2">
+                <img src={Logo} alt="Logo Image" className="w-16" />
+                <div className="text-2xl font-bold pl-1 ">
+                    LIFTEN
+                </div>
+            </div>
+            {MenuItem("DashBoard", <MdOutlineDashboard />, )}
+            {MenuItem("Products", <MdProductionQuantityLimits />, )}
+        </div>
+    </>
+}
--- front-end/src/pages/admin/Dashboard.jsx
+++ front-end/src/pages/admin/Dashboard.jsx
@@ -1,6 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AdminHome from './AdminHome';
+import SideBar from "../../components/sidebar/index.jsx"
 
-export function Dashboard() {
-    return (
-        <div>Dashboard Page</div>
-    );
-}
+const Dashboard = () => {
+  return (
+    <>
+      <div className='flex main'>
+            <div className='w-[20%] sidebarWrapper'>
+                <SideBar />
+            </div>
+
+            {/* Internal Routes for Dashboard page */}
+            <section className=' w-[85%]'>
+                <Routes>
+                    <Route path="home" element={<AdminHome />} />
+                    <Route path="manage" element={<div>Manage</div>} />
+                    <Route path="bill" element={<div>bill</div>} />
+                </Routes>
+            </section>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
