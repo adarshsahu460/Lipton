8dd3480d7839f27d881c465d58146ac3aa833a02
AdarshSahu <100473287+adarshsahu460@users.noreply.github.com>
2024-07-24 00:24:40 +0530

    Update main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -6,8 +6,8 @@ import './index.css'
 import {ToastContainer} from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
     <ToastContainer />
-  </StrictMode>,
+  </>,
 )
